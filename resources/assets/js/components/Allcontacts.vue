3f6ba13499ae29df26252a1c6ede88bec59aafd0
xuedoris <xuedoris0106@hotmail.com>
2017-05-30 17:10:12 -0400

    ajax retrieve data

--- resources/assets/js/components/Allcontacts.vue
+++ resources/assets/js/components/Allcontacts.vue
@@ -1,12 +1,29 @@
 <template>
     <div class="panel-body">
-        <h2>Display All Contacts</h2>
-
+        <div class="form-group has-feedback">
+            <label for="search" class="sr-only">Search</label>
+            <input type="text" class="form-control" name="search" placeholder="search" @keyup="">
+            <span class="glyphicon glyphicon-search form-control-feedback"></span>
+        </div>
+        <div class="row" v-for="contact in contacts">
+            <div class="col-md-4">{{contact.contactname}}</div>
+            <div class="col-md-8">phone number: {{contact.phonenumber}}</div>
+        </div>
     </div>
 </template>
 
 <script>
     export default {
-
+        data: function () {
+            return {
+                contacts: {}
+            }
+        },
+        created: function(){
+            var self = this;
+            axios.get('/contacts').then(function (response) {
+                self.contacts = response.data;
+            });
+        }
     }
 </script>
